cd31342305904810078c60b0124c4797e7c1001d
Nikita Kalinskiy <32264994+nkalinskiy@users.noreply.github.com>
2023-05-10 17:06:29 +0400

    Update and rename ProductionEventAggregator.scala to ProductionEventsAggregator.scala

--- scala/beverages-factory/ProductionEventsAggregator.scala
+++ scala/beverages-factory/ProductionEventsAggregator.scala
@@ -1,19 +1,19 @@
 import cats.effect.kernel.Ref
 import cats.effect.IO
 
-trait ProductionEventAggregator {
+trait ProductionEventsAggregator {
   // Метод принимает результат производтсва одного напитка и сохраняет его в общий список всех событий со всех фабрик
-  def receiveProductionEvent(productionEvent: ProductionEvent): IO[Unit]
+  def saveProductionEvent(productionEvent: ProductionEvent): IO[Unit]
 
   // Получить список всех событий, которые были сохранены в агрегаторе
   def getAllEvents(): IO[List[ProductionEvent]]
 }
 
-object ProductionEventAggregator {
+object ProductionEventsAggregator {
   private class Impl(state: Ref[IO, List[ProductionEvent]])
-      extends ProductionEventAggregator {
+      extends ProductionEventsAggregator {
 
-    override def receiveProductionEvent(
+    override def saveProductionEvent(
         productionResult: ProductionEvent
     ): IO[Unit] =
       state.update(_ :+ productionResult)
@@ -25,6 +25,6 @@ object ProductionEventAggregator {
   // Метод для создания аггрегатора результатов производства.
   // Вызывать его нужно один раз в мейне программы, чтобы был всего один объект на всю программу.
   // Ref это аналог AtomicRef, только из ФП
-  def makeAggregator: IO[ProductionEventAggregator] =
+  def makeAggregator: IO[ProductionEventsAggregator] =
     Ref[IO].of(List.empty[ProductionEvent]).map(new Impl(_))
 }
