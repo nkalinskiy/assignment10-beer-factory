d95a74e54b7918eeda0a9f906a96e576e18deba8
Nikita Kalinskiy <32264994+nkalinskiy@users.noreply.github.com>
2023-05-10 16:56:56 +0400

    Create ProductionEventAggregator.scala

---
+++ scala/beverages-factory/ProductionEventAggregator.scala
@@ -0,0 +1,27 @@
+trait ProductionEventAggregator {
+  // Метод принимает результат производтсва одного напитка и сохраняет его в общий список всех событий со всех фабрик
+  def receiveProductionEvent(productionEvent: ProductionEvent): IO[Unit]
+
+  // Получить список всех событий, которые были сохранены в агрегаторе
+  def getAllEvents(): IO[List[ProductionEvent]]
+}
+
+object ProductionEventAggregator {
+  private class Impl(state: Ref[IO, List[ProductionEvent]])
+      extends ProductionEventAggregator {
+
+    override def receiveProductionEvent(
+        productionResult: ProductionEvent
+    ): IO[Unit] =
+      state.update(_ :+ productionResult)
+
+    override def getAllEvents(): IO[List[ProductionEvent]] =
+      state.get
+  }
+
+  // Метод для создания аггрегатора результатов производства.
+  // Вызывать его нужно один раз в мейне программы, чтобы был всего один объект на всю программу.
+  // Ref это аналог AtomicRef, только из ФП
+  def makeAggregator: IO[ProductionEventAggregator] =
+    Ref[IO].of(List.empty[ProductionEvent]).map(new Impl(_))
+}
