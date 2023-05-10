cf67b384cffe5d784f60089e89414d3dd60e0147
Nikita Kalinskiy <32264994+nkalinskiy@users.noreply.github.com>
2023-05-10 16:56:26 +0400

    Create ProductionEvent.scala

---
+++ scala/beverages-factory/ProductionEvent.scala
@@ -0,0 +1,21 @@
+import cats.effect.kernel.Ref
+import cats.effect.IO
+
+sealed trait ProductionEvent {
+  def factoryName: String
+}
+object ProductionEvent {
+  // Напиток произведен
+  case class BeverageProduced(factoryName: String) extends ProductionEvent
+  // На заводе произошла одна техническая ошибка
+  case class BeverageTechnicalError(factoryName: String) extends ProductionEvent
+  // Производится починка оборудования
+  case class BeverageProductionRepair(factoryName: String, repairTime: Long)
+      extends ProductionEvent
+  // Починка завершена
+  case class BeverageProductionRepaired(factoryName: String)
+      extends ProductionEvent
+  // Работник Баханов выпил напитки
+  case class BeverageProductionNeglect(factoryName: String)
+      extends ProductionEvent
+}
